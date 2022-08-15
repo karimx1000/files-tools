d03143aa724139c5cab40e5eb421d5829b5e01cf
Axye <65100204+karimx1000@users.noreply.github.com>
2022-08-15 03:10:23 +0200

    Update and rename delete.py to delete-words.py

--- delete-words.py
+++ delete-words.py
@@ -1,7 +1,7 @@
 infile = "combo.txt"
 outfile = "cleaned_file.txt"
 
-delete_list = ["Plan:", "Premium", "Live","country:"]
+delete_list = ["Word1", "Word2", "Word3","Word4"]
 with open(infile) as fin, open(outfile, "w+") as fout:
     for line in fin:
         for word in delete_list:
